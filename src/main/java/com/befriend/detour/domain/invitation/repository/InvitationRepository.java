5314a08f1081aaad2fba2274b499031431c9b9b5
박시현 <sihyun615@naver.com>
2024-07-23 14:30:52 +0900

    [#4] 🎉 add : InvitationRepository 파일 생성

---
+++ src/main/java/com/befriend/detour/domain/invitation/repository/InvitationRepository.java
@@ -0,0 +1,9 @@
+package com.befriend.detour.domain.invitation.repository;
+
+import com.befriend.detour.domain.invitation.entity.Invitation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InvitationRepository extends JpaRepository<Invitation, Long> {
+}

f00e505c78e3d7b60cef65df66bbcb4c0efa1c1a
eunsaem <sweeet0308@gmail.com>
2024-07-23 12:25:47 +0900

    [#9] ✨ feat : 마커 관련 에러코드 추가

--- src/main/java/com/befriend/detour/global/exception/ErrorCode.java
+++ src/main/java/com/befriend/detour/global/exception/ErrorCode.java
@@ -7,12 +7,17 @@ import lombok.Getter;
 @Getter
 public enum ErrorCode {
 
+    // user 관련 오류 처리
     DUPLICATE_NICKNAME(500, "이미 사용중인 닉네임입니다."),
     DUPLICATE_USER_ID(500,"이미 사용중인 ID입니다."),
     DUPLICATE_EMAIL(500,"이미 사용중인 EMAIL입니다."),
     USER_NOT_FOUND(404, "존재하지 않는 사용자입니다."),
     LOGIN_FAIL(404, "로그인에 실패했습니다."),
-    WRONG_HTTP_REQUEST(500, "잘못된 http 요청입니다.");
+    WRONG_HTTP_REQUEST(500, "잘못된 http 요청입니다."),
+
+    // marker 관련 오류 처리
+    MARKER_NOT_FOUND(400, "존재하지 않는 마커입니다."),
+    USER_NOT_MATCH_WITH_MARKER (404, "해당 마커의 작성자가 아닙니다.");
 
     private int status;
     private String message;

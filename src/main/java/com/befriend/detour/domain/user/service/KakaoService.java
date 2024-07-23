91c1d58543f62b7ee40586555c38e661bcb32406
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-07-23 19:22:29 +0900

    [#6] ✨ feat : 필요시 회원가입 시켜주는 로직 구현
    - 카카오톡 이메일과 기존 유저 이메일 비교 로직

--- src/main/java/com/befriend/detour/domain/user/service/KakaoService.java
+++ src/main/java/com/befriend/detour/domain/user/service/KakaoService.java
@@ -1,13 +1,18 @@
 package com.befriend.detour.domain.user.service;
 
 import com.befriend.detour.domain.user.dto.KaKaoUserInfoDto;
+import com.befriend.detour.domain.user.entity.User;
+import com.befriend.detour.domain.user.entity.UserRoleEnum;
+import com.befriend.detour.domain.user.entity.UserStatusEnum;
 import com.befriend.detour.domain.user.repository.UserRepository;
 import com.befriend.detour.global.jwt.JwtProvider;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -16,9 +21,9 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
-import org.springframework.http.HttpHeaders;
 
 import java.net.URI;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -35,14 +40,25 @@ public class KakaoService {
     @Value("${SOCIAL_KAKAO_REDIRECT_URI}")
     private String kakaoRedirectUri;
 
-    public String kakaoLogin(String code) throws JsonProcessingException {
+    public void kakaoLogin(String code, HttpServletResponse response) throws JsonProcessingException {
         // 1. "인가 코드"로 "엑세스 토큰" 요청
         String accessToken = getToken(code);
 
         // 2. 토큰으로 카카오 API 호출 : "엑세스 토큰"으로 "카카오 사용자 정보"가져오기
         KaKaoUserInfoDto kakaoUserInfo = getKakaoUserInfo(accessToken);
 
-        return null;
+        // 3. 필요 시에 회원가입
+        User kakaoUser = registerKakaoUserIfNeeded(kakaoUserInfo);
+
+        // 4. JWT 반환
+        String jwtAccessToken = jwtProvider.createAccessToken(kakaoUser.getNickname(), kakaoUser.getRole());
+        String jwtRefreshToken = jwtProvider.createRefreshToken(kakaoUser.getNickname());
+
+        kakaoUser.updateRefresh(jwtRefreshToken);
+        userRepository.save(kakaoUser);
+
+        response.addHeader(HttpHeaders.AUTHORIZATION, "Bearer " + jwtAccessToken);
+        response.setStatus(HttpServletResponse.SC_OK);
     }
 
     private String getToken(String code) throws JsonProcessingException {
@@ -88,7 +104,7 @@ public class KakaoService {
         // 요청 URL 만들기
         URI uri = UriComponentsBuilder
                 .fromUriString("https://kapi.kakao.com") // 카카오 API 서버 URI
-                .path("/users/info") // 사용자 정보 요청 경로
+                .path("/v2/user/me") // 사용자 정보 요청 경로
                 .encode()
                 .build()
                 .toUri();
@@ -119,7 +135,32 @@ public class KakaoService {
         return new KaKaoUserInfoDto(id, nickname, email);
     }
 
+    // 3. 필요 시에 회원가입
+    private User registerKakaoUserIfNeeded(KaKaoUserInfoDto kakaoUserInfo) {
+        // DB 에 중복된 Kakao Id 가 있는지 확인
+        Long kakaoId = kakaoUserInfo.getId();
+        User kakaoUser = userRepository.findByKakaoId(kakaoId).orElse(null);
 
+        if (kakaoUser == null) {  //DB에 해당 카카오 아이디 없다면 회원가입 진행
+            // 카카오 사용자 email 동일한 email 가진 회원이 있는지 확인
+            String kakaoEmail = kakaoUserInfo.getEmail();
+            User sameEmailUser = userRepository.findByEmail(kakaoEmail).orElse(null);
+            if (sameEmailUser != null) {  //DB에 이미 존재하는 이메일을 가진 회원이 있다면
+                kakaoUser = sameEmailUser;  //같은 회원이라고 덮어씌우기
+                kakaoUser = kakaoUser.kakaoIdUpdate(kakaoId);  //기존 회원정보에 카카오 Id 추가
+            } else {  // 신규 회원가입
+                // password: random UUID
+                String password = UUID.randomUUID().toString();  //password는 UUID를 사용해서 랜덤으로 생성
+                String encodedPassword = passwordEncoder.encode(password);  //암호화
 
+                // email: kakao email
+                String email = kakaoUserInfo.getEmail();
+
+                kakaoUser = new User(email, encodedPassword, kakaoUserInfo.getNickname(), UserStatusEnum.ACTIVE, UserRoleEnum.USER, kakaoId);
+            }
+        }
+
+        return kakaoUser;
+    }
 
 }

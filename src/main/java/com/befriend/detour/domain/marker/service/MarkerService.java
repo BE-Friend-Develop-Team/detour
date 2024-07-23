5dcd5790fb4160b7f5a8b0c3428c8202d6d50554
eunsaem <sweeet0308@gmail.com>
2024-07-23 14:35:27 +0900

    [#9] ✨ feat : 마커 글 생성 관련 공통 메서드 추가
    - 동일한 유저인지 비교하는 메서드 추가
    - 조회 관련 코드와 종합하여 정리
    - 현재 사용할 수 없는 서비스 주석처리

--- src/main/java/com/befriend/detour/domain/marker/service/MarkerService.java
+++ src/main/java/com/befriend/detour/domain/marker/service/MarkerService.java
@@ -23,8 +23,8 @@ public class MarkerService {
 
     private final MarkerRepository markerRepository;
     private final UserService userService;
-    private final DailyPlanService dailyPlanService;
-    private final PlaceService placeService;
+//    private final DailyPlanService dailyPlanService;
+//    private final PlaceService placeService;
 
     // 마커 생성
     public MarkerResponseDto createMarker(String nickname, Long dailPlanId, Long placeId, MarkerRequestDto requestDto) {
@@ -33,8 +33,8 @@ public class MarkerService {
         - 프론트에서 placeId를 넘겨주면 해당 아이디로 place와 연관관계 설정
          */
         User user = getUserByNickname(nickname);
-        //  DailyPlan dailyPlan = dailyPlanService.findDailyPlanById(dailPlanId); // 데일리플랜 서비스 생성 후 수정
-        Place place = placeService.findPlaceById(placeId);
+//        DailyPlan dailyPlan = dailyPlanService.findDailyPlanById(dailPlanId); // 데일리플랜 서비스 생성 후 수정
+//        Place place = placeService.findPlaceById(placeId);
 
         Marker marker = new Marker(requestDto.getLatitude(), requestDto.getLongitude(), dailyPlan, place);
         markerRepository.save(marker);
@@ -42,42 +42,34 @@ public class MarkerService {
         return new MarkerResponseDto(marker);
     }
 
-
+    // 마거 전체 조회
     public List<Marker> getAllBoards(String nickname, Long dailyPlanId) {
 
         User user = getUserByNickname(nickname);
         //  DailyPlan dailyPlan = dailyPlanService.findDailyPlanById(dailyPlanId); // 데일리 플랜 생성 후 수정할 예정
 
         if (!isMarkerExist(dailyPlanId)) {
-            throw new CustomException(ErrorCode.NOT_FOUND_MARKER);
+            throw new CustomException(ErrorCode.MARKER_NOT_FOUND);
         }
 
         return markerRepository.findByDailyPlanId(dailyPlanId);
     }
 
-    private User getUserByNickname(String nickname) {
-        return userService.findUserByNickName(nickname);
-    }
-
-    public boolean isMarkerExist(Long dailyPlanId) {
-
-        List<Marker> markers = markerRepository.findByDailyPlanId(dailyPlanId);
-        return !markers.isEmpty();
-    }
-
     // 마커 글 생성
     @Transactional
     public MarkerResponseDto createMarkerContent(String nickname, Long markerId, MarkerContentRequestDto requestDto) {
-        User user = userService.findUserByNickName(nickname);
+        User user = getUserByNickname(nickname);
         Marker marker = findMarkerById(markerId);
 
-        if (user.getId().equals(marker.getDailyPlan().getSchedule().getUser().getId())) {
+        if (isSameUser(user, marker.getDailyPlan().getSchedule().getUser())){
             marker.updateContent(requestDto);
         } else {
             throw new CustomException(ErrorCode.USER_NOT_MATCH_WITH_MARKER);
         }
+
         return new MarkerResponseDto(marker);
     }
+
 
     // ID로 마커 찾기
     public Marker findMarkerById(Long markerId) {
@@ -85,6 +77,23 @@ public class MarkerService {
                 new CustomException(ErrorCode.MARKER_NOT_FOUND));
     }
 
+    // dailyPlanId로 존재하는 마커인지 찾기
+    public boolean isMarkerExist(Long dailyPlanId) {
+        List<Marker> markers = markerRepository.findByDailyPlanId(dailyPlanId);
+        return !markers.isEmpty();
+    }
 
+    // nickname으로 유저 찾기
+    private User getUserByNickname(String nickname) {
+        return userService.findUserByNickName(nickname);
+    }
+
+    // 동일한 유저인지 비교하기
+    private boolean isSameUser(User user1, User user2) {
+        String nickname1 = user1.getNickname();
+        String nickname2 = user2.getNickname();
+
+        return nickname1.equals(nickname2);
+    }
 
 }

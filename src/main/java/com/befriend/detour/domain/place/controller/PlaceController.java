e5c272923d8ede199caf5c1573e9d6951db83a5d
wondo8449 <wondo8448@gmail.com>
2024-07-25 16:55:41 +0900

    [#39] 🩹 fix : api RequestMapping 경로 수정

--- src/main/java/com/befriend/detour/domain/place/controller/PlaceController.java
+++ src/main/java/com/befriend/detour/domain/place/controller/PlaceController.java
@@ -11,26 +11,26 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/api/place")
 public class PlaceController {
 
     private final PlaceService placeService;
 
-    @PostMapping("/place")
+    @PostMapping
     public ResponseEntity<CommonResponseDto<PlaceResponseDto>> createPlace(@RequestBody PlaceRequestDto placeRequestDto) {
         PlaceResponseDto responseDto = placeService.createPlace(placeRequestDto);
 
         return new ResponseEntity<>(new CommonResponseDto<>(201, "장소 저장에 성공하였습니다. 🎉", responseDto), HttpStatus.CREATED);
     }
 
-    @GetMapping("/place/{placeId}")
+    @GetMapping("/{placeId}")
     public ResponseEntity<CommonResponseDto<PlaceResponseDto>> getPlace(@PathVariable Long placeId) {
         PlaceResponseDto responseDto = placeService.getPlace(placeId);
 
         return new ResponseEntity<>(new CommonResponseDto<>(200, "장소 조회에 성공하였습니다. 🎉", responseDto), HttpStatus.OK);
     }
 
-    @PatchMapping("/place/{placeId}")
+    @PatchMapping("/{placeId}")
     public ResponseEntity<CommonResponseDto<PlaceResponseDto>> updatePlace(@PathVariable Long placeId,
                                                                            @RequestBody PlaceRequestDto placeRequestDto) {
         PlaceResponseDto responseDto = placeService.updatePlace(placeId, placeRequestDto);

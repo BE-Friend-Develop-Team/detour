5db5a77927ad253e4176cea328fed66e34dbdf8f
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-07-25 10:55:36 +0900

    [#27] ✨ feat : Admin이 전체 유저를 불러오는 api 생성

---
+++ src/main/java/com/befriend/detour/domain/user/controller/AdminController.java
@@ -0,0 +1,31 @@
+package com.befriend.detour.domain.user.controller;
+
+import com.befriend.detour.domain.user.dto.ProfileResponseDto;
+import com.befriend.detour.domain.user.service.AdminService;
+import com.befriend.detour.global.dto.CommonResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/api/admin")
+public class AdminController {
+
+    private final AdminService adminService;
+
+    @GetMapping("/users")
+    public ResponseEntity<CommonResponseDto> getAllRecipe(@RequestParam("page") int page) {
+        Pageable pageable = PageRequest.of(page - 1, 10);
+        Page<ProfileResponseDto> profileResponseDtos = adminService.getAllUsers(pageable);
+
+        return ResponseEntity.ok(new CommonResponseDto(200, "전체 사용자" + page +"번 페이지 조회 성공하였습니다. 🎉" , profileResponseDtos));
+    }
+
+}

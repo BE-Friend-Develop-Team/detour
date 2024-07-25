4a5e8bd22c29cc758d4c292c75e8e62e720c99e9
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-07-25 10:54:46 +0900

    [#27] ✨ feat : 모든 유저를 페이징해서 찾는 메서드 구현

--- src/main/java/com/befriend/detour/domain/user/repository/UserRepositoryImpl.java
+++ src/main/java/com/befriend/detour/domain/user/repository/UserRepositoryImpl.java
@@ -1,10 +1,13 @@
 package com.befriend.detour.domain.user.repository;
 
+import com.befriend.detour.domain.user.entity.QUser;
 import com.befriend.detour.domain.user.entity.User;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import static com.befriend.detour.domain.user.entity.QUser.user;
@@ -53,6 +56,21 @@ public class UserRepositoryImpl implements UserRepositoryCustom {
                 .fetchOne();
 
         return Optional.ofNullable(result);
+    }
+
+    @Override
+    public Optional<List<User>> findAllUser(Pageable pageable) {
+        QUser qUser = user;
+
+        List<User> users = jpaQueryFactory
+                .selectFrom(user)
+                .orderBy(qUser.createdAt.desc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+
+        return Optional.ofNullable(users);
     }
 
 }

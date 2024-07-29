dcfb90faaa206ea9b05b71af2f16733948c94bf5
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-07-29 15:49:38 +0900

    [#26] ✨ feat : 해당 유저의 좋아요 엔티티를 모두 들고오는 메서드 구현

--- src/main/java/com/befriend/detour/domain/like/repository/LikeRepositoryCustom.java
+++ src/main/java/com/befriend/detour/domain/like/repository/LikeRepositoryCustom.java
@@ -3,8 +3,10 @@ package com.befriend.detour.domain.like.repository;
 import com.befriend.detour.domain.like.entity.Like;
 import com.befriend.detour.domain.schedule.entity.Schedule;
 import com.befriend.detour.domain.user.entity.User;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -12,4 +14,5 @@ public interface LikeRepositoryCustom {
 
     Optional<Like> findLikeByUserAndSchedule(User user, Schedule schedule);
 
+    Optional<List<Like>> getUserLikedSchedules(User user, Pageable pageable);
 }

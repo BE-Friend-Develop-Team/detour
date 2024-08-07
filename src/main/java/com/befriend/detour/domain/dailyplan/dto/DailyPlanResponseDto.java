9e8a1d65fd9450f7d18d1efd381a611f8db5d16b
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-08-07 18:24:13 +0900

    🩹 fix : 삭제 상태인 마커를 조회 해오지 않도록 수정

--- src/main/java/com/befriend/detour/domain/dailyplan/dto/DailyPlanResponseDto.java
+++ src/main/java/com/befriend/detour/domain/dailyplan/dto/DailyPlanResponseDto.java
@@ -2,6 +2,7 @@ package com.befriend.detour.domain.dailyplan.dto;
 
 import com.befriend.detour.domain.dailyplan.entity.DailyPlan;
 import com.befriend.detour.domain.marker.dto.MarkerResponseDto;
+import com.befriend.detour.domain.marker.entity.MarkerStatusEnum;
 import lombok.Getter;
 
 import java.util.List;
@@ -18,7 +19,7 @@ public class DailyPlanResponseDto {
         this.scheduleId = dailyPlan.getSchedule().getId();
         this.dailyPlanId = dailyPlan.getId();
         this.day = dailyPlan.getDay();
-        this.markerList = dailyPlan.getMarkers().stream().map(MarkerResponseDto::new).toList();
+        this.markerList = dailyPlan.getMarkers().stream().filter(m -> m.getStatus() == MarkerStatusEnum.ACTIVE).map(MarkerResponseDto::new).toList();
     }
 
 }
